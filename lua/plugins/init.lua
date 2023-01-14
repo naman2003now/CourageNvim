f91864f2939df756060d3c74d76c7d8ee0073319
Naman Agrawal <a6.namanagrawal@gmail.com>
2023-01-14 19:32:43 +0530

    Plugin Added: Packer.nvim

---
+++ lua/plugins/init.lua
@@ -0,0 +1,31 @@
+
+local ensure_packer = function()
+	local fn = vim.fn
+	local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+		vim.cmd [[packadd packer.nvim]]
+		return true
+	end
+	return false
+end
+
+local plugins = require('plugins.configs')
+local packer_bootstrap = ensure_packer()
+local status_ok, packer = pcall(require, 'packer')
+if status_ok then
+	packer.startup{function(use)
+		plugins(use)
+		if packer_bootstrap then
+			packer.sync()
+		end
+	end,
+	config = {
+		display = {
+			open_fn = function()
+				return require('packer.util').float({ border = 'rounded' })
+			end
+		}
+	}
+}
+end
